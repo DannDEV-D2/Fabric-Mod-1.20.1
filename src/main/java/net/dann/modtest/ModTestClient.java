42317bdccfeb436a42ea039f7c6e49c169eb1614
Daniel <danieldevtop@gmail.com>
2024-12-24 04:42:18 -0300

    a

---
+++ src/main/java/net/dann/modtest/ModTestClient.java
@@ -0,0 +1,11 @@
+package net.dann.modtest;
+
+import  net.fabricmc.api.ClientModInitializer;
+
+public class ModTestClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
